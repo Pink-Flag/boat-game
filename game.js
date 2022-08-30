4dd3326f11831d8d8d300ee14f268ecae71da554
Kristina Petrova <kristina.petrova13@gmail.com>
2022-08-30 17:09:20 +0100

    change pattern of swirlpool

--- game.js
+++ game.js
@@ -65,8 +65,8 @@ class Boat extends EngineObject {
   }
 
   calculateMoveSpeed() {
-    moveSpeed = Math.floor(Math.abs(this.velocity.x + this.velocity.y * 100));
-    console.log(moveSpeed);
+    moveSpeed =
+      Math.round(Math.abs(this.velocity.x + this.velocity.y * 100) * 10) / 10;
   }
 
   whirlpool(obsticlePos) {
@@ -111,7 +111,7 @@ class Boat extends EngineObject {
       1,
       PI,
       0.05, // damping, angleDamping, gravityScale, particleCone, fadeRate,
-      0.5,
+      0.1,
       1 // randomness, collide, additive, randomColorLinear, renderOrder
     );
   }
@@ -205,8 +205,8 @@ class Obstacle extends EngineObject {
     this.yStart = this.pos.y;
     this.t = 0;
     this.dt = Math.PI / 1000;
-    this.xrad = 10;
-    this.yrad = 5;
+    this.xrad = 15;
+    this.yrad = 10;
   }
   moveObstacle() {
     this.pos.x = this.xStart + this.xrad * Math.sin(this.t + Math.PI / 2);
@@ -324,8 +324,8 @@ function gameUpdate() {
   boatPos = boat.pos;
   boat.moveBoat();
   boat.calculateMoveSpeed();
-  if(moveSpeed > 2) {
-    boat.trail(moveSpeed)
+  if (moveSpeed > 0.4) {
+    boat.trail(moveSpeed / 10);
   }
   if (!enemy) {
     enemy = new Enemy(vec2(levelSize.x - 30, levelSize.y / 2 + 10), 0);
